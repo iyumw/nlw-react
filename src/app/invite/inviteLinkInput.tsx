166c0ebd9ebf7421921f7144347688cfe5e04171
Isis Yume Okamoto <isis.yume2508@gmail.com>
2025-02-22 18:13:12 -0300

    :sparkles: feat: criar página de invite e adicionar validações no formulário

---
+++ src/app/invite/inviteLinkInput.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Copy, Link } from 'lucide-react'
+import { IconButton } from '../../components/IconButton'
+import { InputField, InputIcon, InputRoot } from '../../components/Input'
+
+interface InviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+    alert('Link copiado para a área de transferência!')
+  }
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+
+      <InputField readOnly defaultValue={inviteLink} />
+
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
+}
