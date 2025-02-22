58e1b05864395394f4a3f478ca4f6954f7b5b9d9
Isis Yume Okamoto <isis.yume2508@gmail.com>
2025-02-22 00:17:08 -0300

    :tada: Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // Se estiver usando o diretório src
+      "./public/index.html", // Se necessário para HTML estático
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
